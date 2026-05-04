2cb5cf3c688a335be1cfafb32a4864a55a1ff224
abdo6102004amr <abdoamr6102004@gmail.com>
2026-05-05 02:07:13 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,15 +1,6 @@
 import mysql from "mysql2/promise";
-import dotenv from "dotenv";
 
-dotenv.config();
-
-const db = await mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-});
+const db = await mysql.createConnection(process.env.DB_URL);
 
 console.log("DB Connected ✅");
 
