0233940144ed62da57cca520d30bcd3841ea4bdb
abdoamr <abdooamrr2@gmail.com>
2026-05-05 01:58:32 +0300

    fix

--- server.js
+++ server.js
@@ -11,15 +11,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/uploads", express.static("uploads"));
+// ❗ مش محتاج uploads بعد Supabase
+// app.use("/uploads", express.static("uploads"));
 
-// ✅ routes لازم هنا
 app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/promo", promoRoutes);
 app.use("/api/reviews", reviewRoutes);
 
-// ✅ listen لوحده
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
