50538ecad43dc80ba97017c704df1daea14ae8b6
abdo6102004amr <abdoamr6102004@gmail.com>
2026-05-06 23:11:56 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,12 @@
 import mysql from "mysql2/promise";
 
-const db = await mysql.createConnection(process.env.DB_URL);
+const db = mysql.createPool({
+  uri: process.env.DB_URL,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
 
-console.log("DB Connected ✅");
+console.log("DB Pool Connected ✅");
 
 export default db;
